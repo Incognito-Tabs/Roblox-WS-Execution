19160f9b1b16a67e2c815e53a8cd2e06bc07cd19
lncoognito <incognito.original.main@gmail.com>
2023-08-31 00:27:05 +0300

    Update

---
+++ Loadstring.lua
@@ -0,0 +1,4 @@
+local Success, Data			= pcall(game.HttpGet, game, "https://raw.githubusercontent.com/lncoognito/Roblox-WS/main/Client.lua")
+local Success, Function 	= pcall(loadstring, Data)
+
+Function()
