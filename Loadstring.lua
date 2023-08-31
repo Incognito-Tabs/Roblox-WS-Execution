50b78921b5d161538c560ad64bcb4d15a5229192
lncoognito <incognito.original.main@gmail.com>
2023-08-31 03:25:47 +0300

    Publish

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,3 +1,5 @@
+repeat task.wait() until game:IsLoaded()
+
 local Success, Data			= pcall(game.HttpGet, game, "https://raw.githubusercontent.com/lncoognito/Roblox-WS/main/Client.lua")
 local Success, Function 	= pcall(loadstring, Data)
 
