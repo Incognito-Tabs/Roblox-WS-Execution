af809a438663d2f76d87f580943933b148417659
Incognito <75094867+lncoognito@users.noreply.github.com>
2024-06-27 19:22:38 +0300

    Update Client.lua

--- Client.lua
+++ Client.lua
@@ -4,15 +4,15 @@ local SMethod 						= (WebSocket and WebSocket.connect)
 
 if not SMethod then return Client:Kick("Executor is too shitty.") end
 
-local Main 							= function()
-	local Success, WebSocket 		= pcall(SMethod, "ws://localhost:9000/")
-    local Closed                    = false
+local Main 						= function()
+	local Success, WebSocket 			= pcall(SMethod, "ws://localhost:9000/")
+    	local Closed                    		= false
 
 	if not Success then return end
 
 	WebSocket:Send(Services.HttpService:JSONEncode({
-		Method						= "Authorization",
-		Name						= Client.Name
+		Method					= "Authorization",
+		Name					= Client.Name
 	}))
 
 	WebSocket.OnMessage:Connect(function(Unparsed)
@@ -22,8 +22,8 @@ local Main 							= function()
 			local Function, Error 	= loadstring(Parsed.Data)
 
 			if Error then return WebSocket:Send(Services.HttpService:JSONEncode({
-				Method				= "Error",
-				Message				= Error
+				Method			= "Error",
+				Message			= Error
 			}))	end
 			
 			Function()
@@ -32,15 +32,14 @@ local Main 							= function()
 
     -- i fucking hate you electron
 	-- WebSocket.OnClose:Wait()
-
 	WebSocket.OnClose:Connect(function()
-        Closed                       = true
-    end)
+        	Closed                       		= true
+    	end)
 
-    repeat task.wait() until Closed
+    repeat wait() until Closed
 end
 
-while task.wait(1) do
-	local Success, Error 			= pcall(Main)
+while wait(1) do
+	local Success, Error 				= pcall(Main)
 	if not Success then print(Error) end
 end
