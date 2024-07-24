5d09eefc4cfa5eff5e4180538fe1309a8b576f70
Incognito <75094867+Incognito-Tabs@users.noreply.github.com>
2024-07-24 23:40:27 +0300

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,6 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 
-local Success, Data			= pcall(game.HttpGet, game, "https://raw.githubusercontent.com/lncoognito/Roblox-WS/main/Client.lua")
-local Success, Function 	= pcall(loadstring, Data)
+local Success, Data			    = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/Incognito-Tabs/Roblox-WS/main/Client.lua")
+local Success, Function 	  = pcall(loadstring, Data)
 
 Function()
