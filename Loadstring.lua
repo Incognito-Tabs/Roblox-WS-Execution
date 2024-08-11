8a325680c44ab56a2b0a39e0a378062dbbc9373c
Incognito <75094867+Incognito-Tabs@users.noreply.github.com>
2024-08-12 02:46:55 +0300

    Delete Loadstring.lua

--- Loadstring.lua
+++
@@ -1,6 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-
-local Success, Data          = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/Incognito-Tabs/Roblox-WS-Execution/main/Client.lua")
-local Success, Function      = pcall(loadstring, Data)
-
-Function()
