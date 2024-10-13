c5d4965f09227739900bd059941859e9012381f7
Incognito <75094867+Incognito-Tabs@users.noreply.github.com>
2024-10-13 20:02:50 +0300

    Removed Rant - Wave Fix

--- Client.lua
+++ Client.lua
@@ -1,12 +1,12 @@
-local Services						= setmetatable({}, { __index = function(Self, Key) return game.GetService(game, Key) end })
-local Client						= Services.Players.LocalPlayer
-local SMethod						= (WebSocket and WebSocket.connect)
+local Services							= setmetatable({}, { __index = function(Self, Key) return game.GetService(game, Key) end })
+local Client							= Services.Players.LocalPlayer
+local SMethod							= (WebSocket and WebSocket.connect)
 
 if not SMethod then return Client:Kick("Executor is too shitty.") end
 
 local Main							= function()
-	local Success, WebSocket		= pcall(SMethod, "ws://localhost:9000/")
-    local Closed					= false
+	local Success, WebSocket				= pcall(SMethod, "ws://localhost:9000/")
+    	local Closed						= false
 
 	if not Success then return end
 
@@ -16,10 +16,10 @@ local Main							= function()
 	}))
 
 	WebSocket.OnMessage:Connect(function(Unparsed)
-		local Parsed				= Services.HttpService:JSONDecode(Unparsed)
+		local Parsed					= Services.HttpService:JSONDecode(Unparsed)
 		
 		if (Parsed.Method == "Execute") then
-			local Function, Error	= loadstring(Parsed.Data)
+			local Function, Error			= loadstring(Parsed.Data)
 
 			if Error then return WebSocket:Send(Services.HttpService:JSONEncode({
 				Method				= "Error",
@@ -30,17 +30,15 @@ local Main							= function()
 		end
 	end)
 
-    -- i fucking hate you electron
-	-- WebSocket.OnClose:Wait()
-	WebSocket.OnClose:Connect(function()
-        Closed						= true
+    WebSocket.OnClose:Connect(function()
+        Closed							= true
     end)
 
     repeat wait() until Closed
 end
 
 while wait(1) do
-	local Success, Error			= pcall(Main)
+	local Success, Error					= pcall(Main)
 
 	if not Success then print(Error) end
 end
